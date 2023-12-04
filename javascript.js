e342668707ffb14b21d5865d3f02cbc47a25ee6f
mlmeier <143577737+mlmeier@users.noreply.github.com>
2023-12-04 07:56:38 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function toggleSubMenu(submenuId) {
+    var submenu = document.getElementById(submenuId);
+    submenu.style.display = (submenu.style.display === 'none' || submenu.style.display === '') ? 'block' : 'none';
+    var nav = document.querySelector(nav);
+    
+  }
